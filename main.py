c2d8cc6db5cac32706a9badfc3c243c03eb24221
666 <1226887815@qq.com>
2024-10-11 12:32:06 +0800

    B员工修改

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 
-current_p = "C"
+current_p = "Q"
 
 def func_A():
     print("A员工创建函数1")
 
 def fun_B():
     print("B员工创建函数2")
+    print("B员工修改函数2")
     print("B员工修改函数2")
     print("B员工修改函数2")
 
